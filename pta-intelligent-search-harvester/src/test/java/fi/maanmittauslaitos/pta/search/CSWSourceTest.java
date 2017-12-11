7dc3aba9e60d3f4b6848f931c0421667043a7645
Sampo Savolainen <sampo.savolainen@spatineo.com>
2017-12-11 14:34:14 +0200

    Refaktoroitu harvestointia jotta saadaan luotua WFS-harvesteri

--- pta-intelligent-search-harvester/src/test/java/fi/maanmittauslaitos/pta/search/CSWSourceTest.java
+++ pta-intelligent-search-harvester/src/test/java/fi/maanmittauslaitos/pta/search/CSWSourceTest.java
@@ -4,11 +4,12 @@ import java.io.InputStream;
 
 import org.apache.commons.io.IOUtils;
 
+import fi.maanmittauslaitos.pta.search.csw.CSWHarvesterSource;
 import fi.maanmittauslaitos.pta.search.csw.HarvesterSource;
 
 public class CSWSourceTest {
 	public static void main(String[] args) throws Exception {
-		HarvesterSource source = new HarvesterSource();
+		HarvesterSource source = new CSWHarvesterSource();
 		source.setBatchSize(10);
 		source.setOnlineResource("http://paikkatietohakemisto.fi/geonetwork/srv/en/csw");
 		
