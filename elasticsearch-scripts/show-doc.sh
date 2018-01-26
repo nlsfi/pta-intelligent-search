422df73740567706cde1d9a8b9276c1b62b1e5dc
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-01-26 11:49:29 +0200

    Lisätty vaihtoehtoinen hakulause, joka hakee useamman dokumentin annetuilla hakuehdoilla

--- elasticsearch-scripts/show-doc.sh
+++ elasticsearch-scripts/show-doc.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -XGET "http://localhost:9200/catalog/_search?size=1&pretty" -H 'Content-Type: application/json' -d' { "query": { "query_string": { "default_field": "abstract_maui_uri", "query": "http" } } }'
+#curl -XGET "http://localhost:9200/catalog/_search?size=1500&pretty" -H 'Content-Type: application/json' -d' { "query": { "query_string": { "default_field": "annotoidut_avainsanat_uri", "query": "http" } } }'
+curl -XGET "http://localhost:9200/catalog/_search?size=1&pretty" -H 'Content-Type: application/json' -d' { }'
+
