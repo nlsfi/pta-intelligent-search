ffa9a40fda3260c07272b91f3d015fde9f52ad50
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-01-26 11:45:37 +0200

    Utility to give labels to terms

---
+++ pta-intelligent-search-api/src/test/java/fi/maanmittauslaitos/pta/search/api/PrintTerms.java
@@ -0,0 +1,50 @@
+package fi.maanmittauslaitos.pta.search.api;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.vocabulary.SKOS;
+
+import fi.maanmittauslaitos.pta.search.text.RDFTerminologyMatcherProcessor;
+import fi.maanmittauslaitos.pta.search.text.stemmer.Stemmer;
+
+/**
+ * Read IRI's from the command prompt and display their pref labels
+ * 
+ * @author v2
+ *
+ */
+public class PrintTerms {
+	public static void main(String[] args) throws Exception {
+		
+		ApplicationConfiguration config = new ApplicationConfiguration();
+		
+		Model model = config.terminologyModel();
+		Stemmer stemmer = config.stemmer();
+		RDFTerminologyMatcherProcessor terminologyMatcher = config.terminologyMatcher(model, stemmer, Arrays.asList(SKOS.PREF_LABEL));
+		
+		Map<String, String> iriToString = terminologyMatcher.getReverseDict();
+
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String line;
+		while ( (line = br.readLine()) != null) {
+			
+			Pattern p = Pattern.compile("https?://[^\"'#? ,]+");
+			
+			Matcher m = p.matcher(line);
+			while (m.find()) {
+				String iri = m.group();
+				
+				System.out.println(iri + " => "+iriToString.get(iri));
+			}
+		}
+	}
+}
+
