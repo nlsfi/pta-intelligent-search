302ee1e928caaa524fd3cce866f0a28c46b45ffa
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-01-26 11:53:00 +0200

    Poistettu vahingossa commitoitua tauhkaa

--- pta-intelligent-search-harvester/src/main/java/fi/maanmittauslaitos/pta/search/Harvester.java
+++ pta-intelligent-search-harvester/src/main/java/fi/maanmittauslaitos/pta/search/Harvester.java
@@ -1,7 +1,6 @@
 package fi.maanmittauslaitos.pta.search;
 
 import java.io.InputStream;
-import java.util.List;
 
 import fi.maanmittauslaitos.pta.search.index.DocumentSink;
 import fi.maanmittauslaitos.pta.search.index.DocumentSink.IndexResult;
@@ -23,36 +22,16 @@ public class Harvester {
 			if (is == null) {
 				System.out.println("Source is null, skipping, or stopping");
 				return;
-				//continue;
 			}
 			try {
 				Document doc = processor.processDocument(is);
 				
-				//System.out.println(doc.getFields());
-				
-				List<String> tmp = doc.getListValue("onlineResource", String.class);
-				if (tmp != null && tmp.size() != 0) {
-					boolean isService = doc.isFieldTrue("isService");
-					boolean isDataset = doc.isFieldTrue("isDataset");
-					String type;
-					if (isService && isDataset) {
-						type = "BOTH!?!";
-					} else if (isService) {
-						type = "service";
-					} else if (isDataset) {
-						type = "dataset";
-					} else {
-						type = "UNKNOWN!?";
-					}
-					System.out.println(doc.getFields().get("@id")+" ("+type+"): "+tmp);
-				}
-				
 				System.out.println(doc.getFields().get("abstract_maui_uri"));
-			
+				 
 				//if (true) return;
 				
 				IndexResult result = sink.indexDocument(doc);
-				//System.out.println(" => "+result);
+				System.out.println(" => "+result);
 			} finally {
 				is.close();
 			}
