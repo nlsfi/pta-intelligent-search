0b74d563868293098b0ee5f3946c73ce05a2afd4
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-02-22 10:34:19 +0200

    Siivottu pois turhaa koodia ja lisätty kommentti miten todellisuudessa skeemat pitäisi tulkata

--- pta-intelligent-search-harvester/src/main/java/fi/maanmittauslaitos/pta/search/xpath/MockWFSFeatureTypeFieldExtractorConfiguration.java
+++ pta-intelligent-search-harvester/src/main/java/fi/maanmittauslaitos/pta/search/xpath/MockWFSFeatureTypeFieldExtractorConfiguration.java
@@ -4,15 +4,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathException;
-import javax.xml.xpath.XPathFactory;
 
 import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
@@ -23,6 +19,10 @@ import org.w3c.dom.NodeList;
  * of reading the data from the metadata, this uses a configured location from where it loads
  * the names of the fields.
  * 
+ * The real implementation must be able to load the XML schema of a feature type taking into consideration
+ * the fact that a schema might point to another schema etc. So xpath extraction is not really the 
+ * correct way of proceeding, instead a proper schema libraby is required.
+ * 
  * @author v2
  *
  */
@@ -31,12 +31,7 @@ public class MockWFSFeatureTypeFieldExtractorConfiguration extends AbstractField
 	
 	// Injected via Spring
 	private String idXPathExpression = "//gmd:fileIdentifier/*/text()";
-	private Map<String, String> gmlSchemaNamespaces = new HashMap<>();
 	private Map<String, List<String>> injectedFieldsById = new HashMap<>();
-	
-	// Lazy initialized
-	private DocumentBuilder gmlSchemaDocumentBuilder;
-	private XPath gmlSchemaXPath;
 	
 	public void setIdXPathExpression(String idXPathExpression) {
 		this.idXPathExpression = idXPathExpression;
@@ -46,48 +41,12 @@ public class MockWFSFeatureTypeFieldExtractorConfiguration extends AbstractField
 		return idXPathExpression;
 	}
 	
-	public Map<String, String> getGmlSchemaNamespaces() {
-		return gmlSchemaNamespaces;
-	}
-	
-	public void setGmlSchemaNamespaces(Map<String, String> gmlSchemaNamespaces) {
-		gmlSchemaDocumentBuilder = null;
-		gmlSchemaXPath = null;
-		
-		this.gmlSchemaNamespaces = gmlSchemaNamespaces;
-	}
-	
 	public void setInjectedFieldsById(Map<String, List<String>> injectedFieldsById) {
 		this.injectedFieldsById = injectedFieldsById;
 	}
 	
 	public Map<String, List<String>> getInjectedFieldsById() {
 		return injectedFieldsById;
-	}
-	
-	/**
-	 * Only required once we really parse schema documents, now we'll just take it easy.
-	 * 
-	 * @throws ParserConfigurationException
-	 */
-	private void ensureXmlTools() throws ParserConfigurationException {
-		if (gmlSchemaXPath != null && gmlSchemaDocumentBuilder != null) {
-			return;
-		}
-		
-		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-		builderFactory.setNamespaceAware(true);
-		
-		gmlSchemaDocumentBuilder = builderFactory.newDocumentBuilder();
-		
-
-		HashmapNamespaceContextImpl nsContext = new HashmapNamespaceContextImpl();
-		for (Entry<String, String> e : getGmlSchemaNamespaces().entrySet()) {
-			nsContext.registerNamespace(e.getKey(), e.getValue());
-		}
-		
-		gmlSchemaXPath = XPathFactory.newInstance().newXPath();
-		gmlSchemaXPath.setNamespaceContext(nsContext);
 	}
 	
 	@Override
@@ -101,8 +60,6 @@ public class MockWFSFeatureTypeFieldExtractorConfiguration extends AbstractField
 			
 			String id = nodeList.item(0).getNodeValue();
 			
-			ensureXmlTools();
-			
 			// Load document from wherever and then parse it etc...
 			
 			List<String> fields = getInjectedFieldsById().get(id);
@@ -112,7 +69,7 @@ public class MockWFSFeatureTypeFieldExtractorConfiguration extends AbstractField
 			}
 			
 			return fields;
-		} catch(XPathException | ParserConfigurationException e) {
+		} catch(XPathException e) {
 			throw new DocumentProcessingException(e);
 		}
 	}
