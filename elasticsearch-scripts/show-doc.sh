584748cb32ebf721b584e20a6b6f7a03e9f9aec8
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-06-05 09:40:17 +0300

    Remove old show-doc (replaced by metashow) to avoid mistakes

--- elasticsearch-scripts/show-doc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -XGET "http://localhost:9200/catalog/_search?size=1&pretty" -H 'Content-Type: application/json' -d' { }'
-#curl -XGET "http://localhost:9200/catalog/_search?size=100&pretty" -H 'Content-Type: application/json' -d' { }'
-
