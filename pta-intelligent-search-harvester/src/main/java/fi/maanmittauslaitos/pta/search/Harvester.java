8d9254f09e2b332a238d61401f50d732c26c161e
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-06-29 15:05:44 +0300

    Store indexed documents on disk for testing purposes

--- pta-intelligent-search-harvester/src/main/java/fi/maanmittauslaitos/pta/search/Harvester.java
+++ pta-intelligent-search-harvester/src/main/java/fi/maanmittauslaitos/pta/search/Harvester.java
@@ -1,15 +1,21 @@
 package fi.maanmittauslaitos.pta.search;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import fi.maanmittauslaitos.pta.search.documentprocessor.Document;
 import fi.maanmittauslaitos.pta.search.documentprocessor.DocumentProcessor;
 import fi.maanmittauslaitos.pta.search.index.DocumentSink;
 import fi.maanmittauslaitos.pta.search.index.DocumentSink.IndexResult;
+import fi.maanmittauslaitos.pta.search.metadata.ISOMetadataFields;
 
 @SpringBootApplication
 public class Harvester implements CommandLineRunner {
@@ -28,6 +34,8 @@ public class Harvester implements CommandLineRunner {
 		
 		DocumentSink sink = config.getDocumentSink();
 		
+		boolean store = args.length > 0 && args[0].equals("store");
+		
 		int updated = 0;
 		int inserted = 0;
 		
@@ -38,6 +46,10 @@ public class Harvester implements CommandLineRunner {
 			}
 			try {
 				Document doc = processor.processDocument(is);
+				
+				if (store && (updated + inserted) < 100) {
+					writeDocumentToFile(doc);
+				}
 				
 				IndexResult result = sink.indexDocument(doc);
 				switch(result) {
@@ -56,6 +68,15 @@ public class Harvester implements CommandLineRunner {
 		
 		System.out.println("Inserted "+inserted+" documents");
 		System.out.println("Updated "+updated+" documents");
+	}
+
+	private void writeDocumentToFile(Document doc) throws IOException, JsonProcessingException
+	{
+		String id = doc.getValue(ISOMetadataFields.ID, String.class);
+		ObjectMapper objectMapper = new ObjectMapper();
+		try (FileOutputStream out = new FileOutputStream("indexed-documents/"+id+".json")) {
+			objectMapper.writeValue(out, doc.getFields());
+		}
 	}
 
 }
