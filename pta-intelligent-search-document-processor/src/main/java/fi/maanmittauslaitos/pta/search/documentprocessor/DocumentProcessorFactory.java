601993e582a6a0300762d8a98ba9b7298b148804
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-10-03 16:56:15 +0300

    Add test to ensure that term parents are processed to the topmost term and not just the direct parent

--- pta-intelligent-search-document-processor/src/main/java/fi/maanmittauslaitos/pta/search/documentprocessor/DocumentProcessorFactory.java
+++ pta-intelligent-search-document-processor/src/main/java/fi/maanmittauslaitos/pta/search/documentprocessor/DocumentProcessorFactory.java
@@ -20,64 +20,75 @@ public class DocumentProcessorFactory {
 	}
 	
 	public DocumentProcessor createProcessor(DocumentProcessingConfiguration configuration) throws ParserConfigurationException {
-		
-		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-		builderFactory.setNamespaceAware(true);
-		
-		DocumentBuilder builder = builderFactory.newDocumentBuilder();
-		
-
-		HashmapNamespaceContextImpl nsContext = new HashmapNamespaceContextImpl();
-		for (Entry<String, String> e : configuration.getNamespaces().entrySet()) {
-			nsContext.registerNamespace(e.getKey(), e.getValue());
-		}
-		
-		XPath xPath = XPathFactory.newInstance().newXPath();
-		xPath.setNamespaceContext(nsContext);
-
-		Map<String, TextProcessingChain> textProcessingChains = configuration.getTextProcessingChains();
-		
-		return new DocumentProcessor() {
-			
-			@Override
-			public Document processDocument(InputStream is) throws DocumentProcessingException
-			{
-				
-				org.w3c.dom.Document doc;
-				
-				try {
-					doc = builder.parse(is);
-				} catch(Exception e) {
-					throw new DocumentProcessingException(e);
-				}
-				
-				Document ret = new Document();
-				ret.setDom(doc);
-				
-				for (FieldExtractorConfiguration fec : configuration.getFieldExtractors()) {
-					Object value = fec.process(doc, xPath);
-
-					if (fec.getTextProcessorName() != null) {
-						TextProcessingChain chain = textProcessingChains.get(fec.getTextProcessorName());
-						
-						if (chain == null) {
-							throw new IllegalArgumentException("Text processor chain '"+fec.getTextProcessorName()+"' not declared");
-						}
-						
-						@SuppressWarnings("unchecked")
-						List<String> asList = (List<String>)value;
-						
-						value = chain.process(asList);
-					}
-					
-					ret.getFields().put(fec.getField(), value);
-				}
-				
-				return ret;
-			}
-			
-			
-		};
+		return new DocumentProcessorImpl(configuration);
 	}
 
+	public static class DocumentProcessorImpl implements DocumentProcessor {
+		private DocumentProcessingConfiguration configuration;
+		
+		private DocumentBuilder builder;
+		
+		private XPath xPath;
+		
+		private DocumentProcessorImpl(DocumentProcessingConfiguration configuration) throws ParserConfigurationException {
+			this.configuration = configuration;
+			
+			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+			builderFactory.setNamespaceAware(true);
+			
+			this.builder = builderFactory.newDocumentBuilder();
+			
+
+			HashmapNamespaceContextImpl nsContext = new HashmapNamespaceContextImpl();
+			for (Entry<String, String> e : configuration.getNamespaces().entrySet()) {
+				nsContext.registerNamespace(e.getKey(), e.getValue());
+			}
+			
+			this.xPath = XPathFactory.newInstance().newXPath();
+			this.xPath.setNamespaceContext(nsContext);
+
+		}
+		
+		public DocumentProcessingConfiguration getConfiguration() {
+			return configuration;
+		}
+		
+		@Override
+		public Document processDocument(InputStream is) throws DocumentProcessingException
+		{
+			Map<String, TextProcessingChain> textProcessingChains = configuration.getTextProcessingChains();
+			org.w3c.dom.Document doc;
+			
+			try {
+				doc = builder.parse(is);
+			} catch(Exception e) {
+				throw new DocumentProcessingException(e);
+			}
+			
+			Document ret = new Document();
+			ret.setDom(doc);
+			
+			for (FieldExtractorConfiguration fec : configuration.getFieldExtractors()) {
+				Object value = fec.process(doc, xPath);
+
+				if (fec.getTextProcessorName() != null) {
+					TextProcessingChain chain = textProcessingChains.get(fec.getTextProcessorName());
+					
+					if (chain == null) {
+						throw new IllegalArgumentException("Text processor chain '"+fec.getTextProcessorName()+"' not declared");
+					}
+					
+					@SuppressWarnings("unchecked")
+					List<String> asList = (List<String>)value;
+					
+					value = chain.process(asList);
+				}
+				
+				ret.getFields().put(fec.getField(), value);
+			}
+			
+			return ret;
+		}
+		
+	}
 }
---
+++ pta-intelligent-search-harvester/src/test/java/fi/maanmittauslaitos/pta/search/OntologyProcessingTest.java
@@ -0,0 +1,50 @@
+package fi.maanmittauslaitos.pta.search;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import fi.maanmittauslaitos.pta.search.documentprocessor.DocumentProcessingConfiguration;
+import fi.maanmittauslaitos.pta.search.documentprocessor.DocumentProcessor;
+import fi.maanmittauslaitos.pta.search.documentprocessor.DocumentProcessorFactory;
+import fi.maanmittauslaitos.pta.search.documentprocessor.FieldExtractorConfiguration;
+import fi.maanmittauslaitos.pta.search.text.TextProcessingChain;
+
+public class OntologyProcessingTest {
+	private static DocumentProcessorFactory.DocumentProcessorImpl processor;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		processor = (DocumentProcessorFactory.DocumentProcessorImpl)
+				new HarvesterConfig().getCSWRecordProcessor();
+	}
+
+	@Test
+	public void testParentsResolvedUntilTopmostTerm() {
+		DocumentProcessingConfiguration config = processor.getConfiguration();
+		FieldExtractorConfiguration fec = config.getFieldExtractor("abstract_uri_parents");
+		TextProcessingChain chain = config.getTextProcessingChains().get(fec.getTextProcessorName());
+		
+		List<String> output = chain.process(Arrays.asList("kissa"));
+		
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p864")); // Kissaeläimet
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p14567")); // Petoeläimet
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p308")); // Nisäkkäät
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p309")); // Selkärankaiset
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p834")); // Selkäjänteiset
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p2023")); // Eläimet
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p20765")); // Aitotumaiset
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p2024")); // Eliöt
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p174")); // Orgaaninen rakenne
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p1435")); // Fyysinen objekti
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p4762")); // Pysyvä
+		assertTrue(output.contains("http://www.yso.fi/onto/yso/p4205")); // YSO-käsitteet
+		
+		assertEquals(12, output.size());
+	}
+
+}
