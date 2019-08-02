4bccba738efdc77e65f2266543185b31342d1caf
Joona Laine <joona.laine@spatineo.com>
2019-08-02 12:47:03 +0300

    Add initial json based ES quality test base

--- elasticsearch-scripts/metaindex.sh
+++ elasticsearch-scripts/metaindex.sh
@@ -1,31 +1,3 @@
 #!/bin/bash
 
-curl -XPUT 'localhost:9200/pta' -H 'Content-Type: application/json' -d'
-{
-  "mappings": {
-    "metadata": {
-      "properties": {
-        "abstract_uri":              { "type": "keyword" },
-        "abstract_uri_parents":      { "type": "keyword" },
-        "keywords_uri":              { "type": "keyword" },
-        "annotated_keywords_uri":    { "type": "keyword" },
-        "abstract_maui_uri":         { "type": "keyword" },
-        "abstract_maui_uri_parents": { "type": "keyword" },
-        "keywords":                  { "type": "keyword" },
-        "keywordsInspire":           { "type": "keyword" },
-        "topicCategories":           { "type": "keyword" },
-        "distributionFormats":       { "type": "keyword" },
-        "titleFiSort":               { "type": "keyword" },
-        "titleSvSort":               { "type": "keyword" },
-        "titleEnSort":               { "type": "keyword" },
-        "organisations": {
-          "type": "object",
-          "properties": {
-            "organisationName":      { "type": "keyword" }
-          }
-        }
-      }
-    }
-  }
-}'
-
+curl -XPUT 'localhost:9200/pta' -H 'Content-Type: application/json' -d @../pta-intelligent-search-qa/src/test/resources/fi/maanmittauslaitos/pta/search/integration/index.json
--- pta-intelligent-search-qa/src/test/java/fi/maanmittauslaitos/pta/search/integration/SearchTest.java
+++ pta-intelligent-search-qa/src/test/java/fi/maanmittauslaitos/pta/search/integration/SearchTest.java
@@ -1,28 +1,19 @@
 package fi.maanmittauslaitos.pta.search.integration;
 
-import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.*;
-//import static org.junit.Assert.*;
-
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
+import fi.maanmittauslaitos.pta.search.elasticsearch.PTAElasticSearchMetadataConstants;
+import fi.maanmittauslaitos.pta.search.utils.LocalResourceMetadataGenerator;
 import org.apache.commons.io.IOUtils;
 import org.elasticsearch.action.DocWriteResponse.Result;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -30,127 +21,119 @@ import org.elasticsearch.test.ESIntegTestCase;
 import org.elasticsearch.test.ESIntegTestCase.ClusterScope;
 import org.elasticsearch.test.ESIntegTestCase.Scope;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.internal.util.collections.Sets;
 
-import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAllSuccessful;
 
-import fi.maanmittauslaitos.pta.search.elasticsearch.PTAElasticSearchMetadataConstants;
+//import static org.junit.Assert.*;
 
-@Ignore
 @ClusterScope(scope = Scope.SUITE)
 @ThreadLeakScope(ThreadLeakScope.Scope.NONE)
-public class SearchTest extends ESIntegTestCase  {
-	
-	String index = "{\n" + 
-			"  \"mappings\": {\n" + 
-			"    \"metadata\": {\n" + 
-			"      \"properties\": {\n" + 
-			"        \"abstract_uri\":              { \"type\": \"keyword\" },\n" + 
-			"        \"abstract_uri_parents\":      { \"type\": \"keyword\" },\n" + 
-			"        \"keywords_uri\":              { \"type\": \"keyword\" },\n" + 
-			"        \"annotated_keywords_uri\":    { \"type\": \"keyword\" },\n" + 
-			"        \"abstract_maui_uri\":         { \"type\": \"keyword\" },\n" + 
-			"        \"abstract_maui_uri_parents\": { \"type\": \"keyword\" },\n" + 
-			"        \"keywords\":                  { \"type\": \"keyword\" },\n" + 
-			"        \"keywordsInspire\":           { \"type\": \"keyword\" },\n" + 
-			"        \"topicCategories\":           { \"type\": \"keyword\" },\n" + 
-			"        \"distributionFormats\":       { \"type\": \"keyword\" },\n" + 
-			"        \"titleFiSort\":               { \"type\": \"keyword\" },\n" + 
-			"        \"titleSvSort\":               { \"type\": \"keyword\" },\n" + 
-			"        \"titleEnSort\":               { \"type\": \"keyword\" },\n" + 
-			"        \"organisations\": {\n" + 
-			"          \"type\": \"object\",\n" + 
-			"          \"properties\": {\n" + 
-			"            \"organisationName\":      { \"type\": \"keyword\" }\n" + 
-			"          }\n" + 
-			"        }\n" + 
-			"      }\n" + 
-			"    }\n" + 
-			"  }\n" + 
-			"}";
-	
-	@Before
-	public void createIndexAndPopulate() throws IOException
-	{
-		CreateIndexResponse response = client().admin().indices().prepareCreate("pta").setSource(index, XContentType.JSON).get();
-		assertAcked(response);
+@RunWith(com.carrotsearch.randomizedtesting.RandomizedRunner.class)
+public class SearchTest extends ESIntegTestCase {
 
-		ZipInputStream zipFile = new ZipInputStream(SearchTest.class.getResourceAsStream("/test-documents-1.zip"));
+    private Client client;
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        this.client = client();
+    }
+
+    @Before
+    public void createIndexAndPopulate() throws IOException, URISyntaxException {
+        ZipInputStream zipFile2 = new ZipInputStream(LocalResourceMetadataGenerator.class.getResourceAsStream("/fi/maanmittauslaitos/pta/search/integration/generatedResourceMetadata.zip"));
+
+        ZipInputStream zipFile = new ZipInputStream(SearchTest.class.getResourceAsStream("/test-documents-1.zip"));
 
 
-		ZipEntry entry;
-		int nDocs = 0;
-	    while((entry = zipFile.getNextEntry()) != null){
-	    
-	        byte [] buf = new byte[(int)entry.getSize()];
-	        IOUtils.readFully(zipFile, buf);
-	        zipFile.closeEntry();
-	        
-	        Map<String, Object> mapValue = XContentHelper.convertToMap(JsonXContent.jsonXContent, new String(buf), false);
-	        
-	        @SuppressWarnings("rawtypes")
-			String id = ((Collection)mapValue.get("@id")).iterator().next().toString();
-	        
-	        IndexResponse insertResponse = client().prepareIndex("pta", "metadata", id)
-	                .setSource(buf, XContentType.JSON)
-	                .get();
-	        
-	        assertEquals(Result.CREATED, insertResponse.getResult());
-	        nDocs++;
-	    }
-		System.out.println("Inserted "+nDocs+", waiting until all are searchable");
-		assertEquals(100, nDocs);
-		
-		refresh();
-	}
-	
-	@Test
-	public void searchUsingMauiParent() throws Exception {
-		
-		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-		sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
-		sourceBuilder.fetchSource("*", null);
-		
-		BoolQueryBuilder query = QueryBuilders.boolQuery();
-		
-		QueryBuilder tmp = QueryBuilders.termQuery(PTAElasticSearchMetadataConstants.FIELD_ABSTRACT_MAUI_URI_PARENTS, "http://www.yso.fi/onto/yso/p2536");
-		tmp.boost(1.0f);
-		query.should().add(tmp);
-		
-		sourceBuilder.query(query);
-		
-		SearchRequest request = new SearchRequest(PTAElasticSearchMetadataConstants.INDEX);
-		request.types(PTAElasticSearchMetadataConstants.TYPE);
-		request.source(sourceBuilder);
-		
-		System.out.println("query:");
-		System.out.println(query);
-		
-		SearchResponse response = client().search(request).actionGet();
-		
-		assertAllSuccessful(response);
-		
-		System.out.println("response:");
-		System.out.println(response);
-		
-		
-		Set<String> ids = Sets.newSet(
-			"0d37d33d-86bc-49cc-9b4b-4d1f005ace56",
-			"a7889960-2d49-4129-bc6a-7f871710425e",
-			"a959b07c-16ff-4ed0-9a3b-72d6bbc026ff",
-			"c4c69e7f-2b40-45aa-a832-46d9fce8b32c",
-			"cb067ce0-9ec6-432a-8d1a-4a2944fc476a",
-			"ddad3347-05ca-401a-b746-d883d4110180"
-		);
-		
-		assertEquals(ids.size(), response.getHits().getTotalHits());
-		
-		for (SearchHit hit : response.getHits().getHits()) {
-			assertTrue(hit.getId()+" should be one of the six known hits", ids.contains(hit.getId()));
-		}
-	}
-	
+
+        String index = new String(Files.readAllBytes(Paths.get(getClass().getResource("index.json").toURI())));
+        System.out.println(index);
+        CreateIndexResponse response = client().admin().indices().prepareCreate("pta").setSource(index, XContentType.JSON).get();
+        assertAcked(response);
+
+
+
+
+        ZipEntry entry;
+        int nDocs = 0;
+        while ((entry = zipFile.getNextEntry()) != null) {
+
+            byte[] buf = new byte[(int) entry.getSize()];
+            IOUtils.readFully(zipFile, buf);
+            zipFile.closeEntry();
+
+            Map<String, Object> mapValue = XContentHelper.convertToMap(JsonXContent.jsonXContent, new String(buf), false);
+
+            @SuppressWarnings("rawtypes")
+            String id = ((Collection) mapValue.get("@id")).iterator().next().toString();
+
+            IndexResponse insertResponse = client().prepareIndex("pta", "metadata", id)
+                    .setSource(buf, XContentType.JSON)
+                    .get();
+
+            assertEquals(Result.CREATED, insertResponse.getResult());
+            nDocs++;
+        }
+        System.out.println("Inserted " + nDocs + ", waiting until all are searchable");
+        assertEquals(100, nDocs);
+
+        refresh();
+    }
+
+    @Test
+    public void searchUsingMauiParent() throws Exception {
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+        sourceBuilder.fetchSource("*", null);
+
+        String queryStr = new String(Files.readAllBytes(Paths.get(getClass().getResource("1.json").toURI())));
+        sourceBuilder.query(QueryBuilders.wrapperQuery(queryStr));
+
+        SearchRequest request = new SearchRequest(PTAElasticSearchMetadataConstants.INDEX);
+        request.types(PTAElasticSearchMetadataConstants.TYPE);
+        request.source(sourceBuilder);
+
+        System.out.println("Query:\n" + queryStr);
+
+        SearchResponse response = client().search(request).actionGet();
+
+        assertAllSuccessful(response);
+
+        System.out.println("response:");
+        System.out.println(response);
+
+
+        Set<String> ids = Sets.newSet(
+                "0d37d33d-86bc-49cc-9b4b-4d1f005ace56",
+                "a7889960-2d49-4129-bc6a-7f871710425e",
+                "a959b07c-16ff-4ed0-9a3b-72d6bbc026ff",
+                "c4c69e7f-2b40-45aa-a832-46d9fce8b32c",
+                "cb067ce0-9ec6-432a-8d1a-4a2944fc476a",
+                "ddad3347-05ca-401a-b746-d883d4110180"
+        );
+
+        assertEquals(ids.size(), response.getHits().getTotalHits());
+
+        for (SearchHit hit : response.getHits().getHits()) {
+            assertTrue(hit.getId() + " should be one of the six known hits", ids.contains(hit.getId()));
+        }
+    }
+
 }
