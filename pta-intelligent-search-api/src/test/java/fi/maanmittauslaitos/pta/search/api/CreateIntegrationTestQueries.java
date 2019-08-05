d2d85fc2d3593a262daebc7a986c3b55d32f8b2b
Joona Laine <joona.laine@spatineo.com>
2019-08-05 12:53:11 +0300

    Generate test queries in api project to be used in qa

--- pta-intelligent-search-api/src/test/java/fi/maanmittauslaitos/pta/search/api/CreateIntegrationTestQueries.java
+++ pta-intelligent-search-api/src/test/java/fi/maanmittauslaitos/pta/search/api/CreateIntegrationTestQueries.java
@@ -1,9 +1,11 @@
 package fi.maanmittauslaitos.pta.search.api;
 
-import java.io.IOException;
-import java.util.Arrays;
-
+import fi.maanmittauslaitos.pta.search.api.ApplicationConfiguration.MockElasticsearchQueryAPI;
+import fi.maanmittauslaitos.pta.search.api.model.SearchQuery;
+import fi.maanmittauslaitos.pta.search.api.search.HakuKone;
 import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -11,58 +13,122 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import fi.maanmittauslaitos.pta.search.api.ApplicationConfiguration;
-import fi.maanmittauslaitos.pta.search.api.ApplicationConfiguration.MockElasticsearchQueryAPI;
-import fi.maanmittauslaitos.pta.search.api.Language;
-import fi.maanmittauslaitos.pta.search.api.model.SearchQuery;
-import fi.maanmittauslaitos.pta.search.api.search.HakuKone;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CreateIntegrationTestQueries implements ApplicationRunner {
-	private static Logger logger = Logger.getLogger(CreateIntegrationTestQueries.class);
-	
-	@SuppressWarnings("unused")
-	@Autowired
-	private ApplicationConfiguration configuration;
-	
-	@Autowired
-	private MockElasticsearchQueryAPI esQueryAPI;
-	
-	@Autowired
-	HakuKone hakukone;
-	
-	public static void main(String[] args) {
-		System.setProperty("spring.profiles.active", "createIntegrationTestQueries");
-		try {
-			ConfigurableApplicationContext ctx = SpringApplication.run(CreateIntegrationTestQueries.class, args);
-			SpringApplication.exit(ctx);
-		} catch(Exception e) {
-			logger.error("Configuration error", e);
-		}
-	}
+    private static Logger logger = Logger.getLogger(CreateIntegrationTestQueries.class);
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		SearchQuery pyynto = new SearchQuery();
-		pyynto.setQuery(Arrays.asList("suomi"));
-		
-		boolean focusOnRegionalHits = false;
-		String json = convertQueryIntoJSON(pyynto, Language.FI, focusOnRegionalHits);
-		
-		System.out.println("Query as JSON: "+json);
-	}
+    @SuppressWarnings("unused")
+    @Autowired
+    private ApplicationConfiguration configuration;
 
-	private String convertQueryIntoJSON(SearchQuery pyynto, Language lang, boolean focusOnRegionalHits) throws IOException
-	{
-		String json;
-		
-		try {
-			hakukone.haku(pyynto, lang, focusOnRegionalHits);
-		} catch(NullPointerException npe) {
-			/* NOP, we expect this to happen as the ES mock returns null */
-		}
+    @Autowired
+    private MockElasticsearchQueryAPI esQueryAPI;
 
-		json = esQueryAPI.getLastQueryAsJSON();
-		return json;
-	}
+    @Autowired
+    HakuKone hakukone;
+
+    public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "createIntegrationTestQueries");
+        try {
+            ConfigurableApplicationContext ctx = SpringApplication.run(CreateIntegrationTestQueries.class, args);
+            SpringApplication.exit(ctx);
+        } catch (Exception e) {
+            logger.error("Configuration error", e);
+        }
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        String outputDir = args.getSourceArgs().length > 0 ? args.getSourceArgs()[0] : "jsontest";
+        File directory = new File(outputDir);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+
+
+        Stream.of(new ConversionHelper("testcase-1", Collections.singletonList("suomi"), false),
+                new ConversionHelper("testcase-2", Arrays.asList("jyväskylä", "tiet"), true),
+                new ConversionHelper("testcase-3", Collections.emptyList(), false),
+                new ConversionHelper("testcase-4", Collections.singletonList("hsl"), false))
+                .forEach(helper -> generateTestCaseQuery(helper.getQueryList(), Paths.get(outputDir, helper.getTestCaseName() + ".json"), helper.isFocusOnRegional()));
+    }
+
+    private void generateTestCaseQuery(List<String> queryList, Path outFile, boolean focusOnRegional) {
+        SearchQuery pyynto = new SearchQuery();
+        pyynto.setQuery(queryList);
+
+        String json = null;
+        try {
+            JSONObject jsonObject = new JSONObject(convertQueryIntoJSON(pyynto, Language.FI, focusOnRegional));
+            json = jsonObject.getJSONObject("query").toString(2);
+
+            Files.write(outFile, json.getBytes());
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Query as JSON: " + json);
+    }
+
+    private String convertQueryIntoJSON(SearchQuery pyynto, Language lang, boolean focusOnRegionalHits) throws IOException {
+        String json;
+
+        try {
+            hakukone.haku(pyynto, lang, focusOnRegionalHits);
+        } catch (NullPointerException npe) {
+            /* NOP, we expect this to happen as the ES mock returns null */
+        }
+
+        json = esQueryAPI.getLastQueryAsJSON();
+        return json;
+    }
+
+    static class ConversionHelper {
+        String testCaseName;
+        List<String> queryList;
+        boolean focusOnRegional;
+
+
+        ConversionHelper(String testCaseName, List<String> queryList, boolean regionalFlag) {
+            this.testCaseName = testCaseName;
+            this.queryList = queryList;
+            this.focusOnRegional = regionalFlag;
+        }
+
+        public String getTestCaseName() {
+            return testCaseName;
+        }
+
+        public void setTestCaseName(String testCaseName) {
+            this.testCaseName = testCaseName;
+        }
+
+        public List<String> getQueryList() {
+            return queryList;
+        }
+
+        public void setQueryList(List<String> queryList) {
+            this.queryList = queryList;
+        }
+
+        public boolean isFocusOnRegional() {
+            return focusOnRegional;
+        }
+
+        public void setFocusOnRegional(boolean focusOnRegional) {
+            this.focusOnRegional = focusOnRegional;
+        }
+    }
 }
