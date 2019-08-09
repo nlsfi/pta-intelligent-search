4960e51e23b422e2fb336b569a04829319f7fc60
Joona Laine <joona.laine@spatineo.com>
2019-08-09 14:30:31 +0300

    Create resource folder for integration tests if needed

--- pta-intelligent-search-harvester/src/main/java/fi/maanmittauslaitos/pta/search/index/LocalArchiveDocumentSink.java
+++ pta-intelligent-search-harvester/src/main/java/fi/maanmittauslaitos/pta/search/index/LocalArchiveDocumentSink.java
@@ -3,6 +3,7 @@ package fi.maanmittauslaitos.pta.search.index;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.maanmittauslaitos.pta.search.documentprocessor.Document;
 import fi.maanmittauslaitos.pta.search.metadata.ISOMetadataFields;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -14,6 +15,8 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class LocalArchiveDocumentSink implements DocumentSink {
+
+    private static Logger logger = Logger.getLogger(LocalArchiveDocumentSink.class);
 
     private String outputArchive;
     private Path localArchiveDocumentSink = null;
@@ -34,7 +37,7 @@ public class LocalArchiveDocumentSink implements DocumentSink {
         try {
             writeDocumentToFile(doc);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Could not write document to file",e);
         }
         return IndexResult.INSERTED;
     }
@@ -51,9 +54,14 @@ public class LocalArchiveDocumentSink implements DocumentSink {
     public int stopIndexing() {
         Path zipFile = Paths.get(outputFileName);
         try {
+            File outputRootDir = zipFile.getParent().toFile();
+            if (!outputRootDir.exists()) {
+                logger.warn("Creating new directory " + outputRootDir);
+                outputRootDir.mkdirs();
+            }
             Files.deleteIfExists(zipFile);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Failed to delete file " + outputFileName, e);
         }
         try(ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(Files.createFile(zipFile)))) {
             Files.walk(localArchiveDocumentSink).filter(path -> !Files.isDirectory(path))
@@ -64,13 +72,13 @@ public class LocalArchiveDocumentSink implements DocumentSink {
                             zipOutputStream.write(Files.readAllBytes(path));
                             zipOutputStream.closeEntry();
                         } catch (IOException e) {
-                            e.printStackTrace();
+                            logger.error("Could not write to zip file", e);
                         }
                     });
 
             localArchiveDocumentSink.toFile().deleteOnExit();
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Failed to write the zip file " + outputFileName, e);
         }
 
         return 0;
