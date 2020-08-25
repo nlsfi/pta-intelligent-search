b4d42cdf0c52b68418c6753dedd7223ac7872472
patrick-alaspaa <patrick.alaspaa@spatineo.com>
2020-08-25 05:30:00 +0300

    Add CORS bean to configuration

--- pta-intelligent-search-api/src/main/java/fi/maanmittauslaitos/pta/search/api/ApplicationConfiguration.java
+++ pta-intelligent-search-api/src/main/java/fi/maanmittauslaitos/pta/search/api/ApplicationConfiguration.java
@@ -39,6 +39,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -528,5 +531,15 @@ public class ApplicationConfiguration {
 		ret.setHintProvider(hintProvider);
 
 		return ret;
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 }
